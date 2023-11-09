764773108d51107118fdbd5507e63fff5dfcf8cc
Wesner-Software-Ausbildung <sergei.pron@wesner-software.de>
2023-11-09 16:07:46 +0100

    Ich gehe weiter. Ich habe den Fehler in zweite teil_ aber ich mache das selbst.

--- src/arrayCalculator/ArrayCalculator.java
+++ src/arrayCalculator/ArrayCalculator.java
@@ -3,12 +3,40 @@ package arrayCalculator;
 
 public class ArrayCalculator {
     public int[] duplicateArrayValues(int[] array) {
+        if (array == null) {
+            return null;
+        }
 
-        return null;
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i] * 2;
+
+        }
+        return result;
     }
 
     public int[] sumArrays(int[] array1, int[] array2) throws IllegalArgumentException {
-        return null;
+
+        if (array1 == null) {
+            return null;
+        }
+
+        int[] result = new int[array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i] * 2;
+        }
+        //return result;
+
+        if (array2 == null) {
+            return null;
+        }
+        int[] result2 = new int[array2.length];
+        for (int i = 0; i < array2.length; i++) {
+            result2[i] = array2.length * 3 + 1;
+        }
+
+
+        return result;
     }
 
     public int[] swapArrays(int[] array) {
